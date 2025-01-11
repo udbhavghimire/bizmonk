1da40d842521a9e34f2a6e6cdc8506b39d41c320
aparg <apargtm@gmail.com>
2025-01-11 15:33:28 +0545

    fix: mobile responsiveness for TextWithContactButton component

--- components/TextWithContactButton.jsx
+++ components/TextWithContactButton.jsx
@@ -3,11 +3,16 @@ import React from "react";
 
 const TextWithContactButton = ({ title, subtitle, textContent, imgSrc }) => {
   return (
-    <div class="flex justify-between max-w-7xl mx-auto items-center px-12 my-20 w-full">
-      <div class="max-w-lg">
+    <div class="flex flex-col sm:flex-row justify-between max-w-7xl mx-auto px-4 sm:px-6 items-center my-20 w-full text-center sm:text-left">
+      <div class="max-w-lg sm:w-1/2">
         <h1 class="text-4xl font-serif mb-6 text-black">{title}</h1>
         <h2 class="text-2xl font-bold mb-4 text-black">{subtitle}</h2>
-        <p class="text-lg mb-4 text-black">{textContent}</p>
+        <img
+          src={imgSrc}
+          alt="Franchise Image"
+          className="w-full mt-5 sm:m-10 rounded-lg sm:hidden"
+        />
+        <p class="text-lg mb-4 text-black mt-5 sm:mt-0">{textContent}</p>
 
         <Link
           href="tel:(647) 123-4567"
@@ -16,11 +21,11 @@ const TextWithContactButton = ({ title, subtitle, textContent, imgSrc }) => {
           Contact us
         </Link>
       </div>
-      <div class="w-1/2">
+      <div class="w-full h-full sm:w-1/2">
         <img
           src={imgSrc}
           alt="Franchise Image"
-          class="w-full m-10 rounded-lg"
+          className="sm:h-[500px] sm:w-[300px] md:h-[500px] md:w-full mt-5 md:m-0 sm:m-10 rounded-lg hidden sm:block object-cover"
         />
       </div>
     </div>
