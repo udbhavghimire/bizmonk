a31ec6a4e97fad64c9830e9496f3243ab6852022
aparg <apargtm@gmail.com>
2025-01-12 22:00:57 +0545

    style: spacing and minor fixes

--- components/PropertyDisplaySection.jsx
+++ components/PropertyDisplaySection.jsx
@@ -8,14 +8,14 @@ const PropertyDisplaySection = ({
   children,
 }) => {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 rounded-md bg-gradient-to-r from-blue-600/15 to-pink-500/15 my-24">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:rounded-md bg-gradient-to-r from-blue-600/15 to-pink-500/15 my-24">
       <div className="my-2 ">
         <div className="flex flex-row items-center justify-between">
           <h3 className="text-xl sm:text-2xl font-semibold w-[100%] sm:w-auto text-black">
             {title}
           </h3>
           <Link href={exploreAllLink || "#"} className="hidden sm:inline">
-            <button className="border-black font-bold border-2 inline px-1 sm:px-3 py-2 rounded-md text-sm sm:text-md text-black">
+            <button className="border-black font-bold border-2 inline px-1 sm:px-3 py-2 rounded-md text-sm sm:text-md text-black hover:scale-105">
               Explore All
             </button>
           </Link>
@@ -26,11 +26,8 @@ const PropertyDisplaySection = ({
       </div>
       {children}
       <div className="flex justify-center">
-        <Link
-          href={exploreAllLink || "#"}
-          className="sm:hidden hover:scale-150"
-        >
-          <button className="border-black font-bold border-2 inline px-1 sm:px-3 py-2 rounded-md text-sm mt-1 sm:text-md ">
+        <Link href={exploreAllLink || "#"} className="sm:hidden">
+          <button className="font-semibold border-2 inline px-3 sm:px-3 py-2 rounded-full text-sm mt-1 sm:text-md hover:scale-105 bg-white text-black">
             Explore All
           </button>
         </Link>
--- components/TextWithContactButton.jsx
+++ components/TextWithContactButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const TextWithContactButton = ({ title, subtitle, textContent, imgSrc }) => {
   return (
-    <div className="flex flex-col sm:flex-row justify-between max-w-7xl mx-auto px-4 sm:px-6 items-center my-20 w-full text-center sm:text-left">
+    <div className="flex flex-col sm:flex-row justify-between max-w-7xl mx-auto px-4 sm:px-6 items-center py-24 w-full text-center sm:text-left">
       <div className="max-w-lg sm:w-1/2">
         <h1 className="text-4xl font-serif mb-6 text-black">{title}</h1>
         <h2 className="text-2xl font-bold mb-4 text-black">{subtitle}</h2>
@@ -12,7 +12,9 @@ const TextWithContactButton = ({ title, subtitle, textContent, imgSrc }) => {
           alt="Franchise Image"
           className="w-full mt-5 sm:m-10 rounded-lg sm:hidden"
         />
-        <p className="text-lg mb-4 text-black mt-5 sm:mt-0">{textContent}</p>
+        <p className="text-md sm:text-lg mb-4 text-black mt-5 sm:mt-0">
+          {textContent}
+        </p>
 
         <Link
           href="tel:(647) 123-4567"
