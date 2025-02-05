6cb542ef865c1c129864a4c60ffcb582a5961574
Udbhav Ghimire <ghimireudbhav@gmail.com>
2025-02-05 20:44:17 +0545

    office-for lease

--- app/offices-for-lease/page.js
+++ app/offices-for-lease/page.js
@@ -1,4 +1,3 @@
-"use client";
 import { useState, useEffect } from "react";
 import Breadcrumb from "@/components/Breadcrumb";
 import citiesData from "@/data/gta-cities.json";
@@ -15,34 +14,8 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function CityOffices() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 20;
-  const [listings, setListings] = useState([]);
-
-  useEffect(() => {
-    const fetchListings = async () => {
-      try {
-        const data = await getOfficeListings({});
-        setListings(data);
-      } catch (error) {
-        console.error("Error fetching listings:", error);
-      }
-    };
-
-    fetchListings();
-  }, []);
-
-  // Calculate pagination
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = listings.slice(indexOfFirstItem, indexOfLastItem);
-  const totalPages = Math.ceil(listings.length / itemsPerPage);
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+export default async function CityOffices() {
+  const data = await getOfficeListings({});
 
   return (
     <div className="">
@@ -52,23 +25,15 @@ export default function CityOffices() {
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3">
-          {currentItems.map((listing) => (
+          {data.map((listing) => (
             <ResaleCard curElem={listing} key={listing.ListingKey} />
           ))}
         </div>
 
-        {listings.length === 0 && (
+        {data.length === 0 && (
           <div className="text-center py-8 text-gray-500">
             No listings found
           </div>
-        )}
-
-        {listings.length > 0 && (
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={handlePageChange}
-          />
         )}
       </div>
     </div>
